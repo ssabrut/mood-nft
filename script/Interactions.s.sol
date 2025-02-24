16c4ae275121d3e092f764ad4306bd5b159948c9
Michael Eco <michael.gunawan2002@gmail.com>
2025-02-24 19:28:09 +0700

    feat: implement interaction for mint nft

---
+++ script/Interactions.s.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
+import {BasicNFT} from "src/BasicNFT.sol";
+
+contract MintBasicNFT is Script {
+    string public constant PUG =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "BasicNFT",
+            block.chainid
+        );
+        mintNFTOnContract(mostRecentlyDeployed);
+    }
+
+    function mintNFTOnContract(address _contractAddress) public {
+        vm.startBroadcast();
+        BasicNFT(_contractAddress).mint(PUG);
+        vm.stopBroadcast();
+    }
+}
