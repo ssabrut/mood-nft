4a77b6b11be858ecacaf1f216156fb5dd9f1c64e
Michael Eco <michael.gunawan2002@gmail.com>
2025-02-25 13:23:12 +0700

    modules

---
+++ script/DeployMoodNFT.s.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {MoodNFT} from "src/MoodNFT.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
+
+contract DeployMoodNFT is Script {
+    function run() external returns (MoodNFT) {}
+
+    function svgToImageURI(
+        string memory _svg
+    ) public pure returns (string memory) {
+        string memory baseURL = "data:image/svg+xml;base64,";
+        string memory svgBase64Encoded = Base64.encode(
+            bytes(string(abi.encodePacked(_svg)))
+        );
+
+        return string(abi.encodePacked(baseURL, svgBase64Encoded));
+    }
+}
